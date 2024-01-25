c2bdfd6d50a4af5caf576c6201d6a65f64254bef
ugochukwumaduakor <umaduakor@hotmail.com>
2024-01-26 01:54:05 +1100

    shortest reverse string solution

---
+++ Reverse_String_4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Reverse_String_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please Type the word you want to reverse and press enter:");
+        Console.WriteLine("\n");
+
+        string userInput = Console.ReadLine();
+        char[] charUserInput = userInput.ToCharArray();
+
+        Array.Reverse(charUserInput);
+
+        string reversedStr = new string(charUserInput);
+
+        Console.Write($"Actual String is : {userInput} \n");
+        Console.Write($"Reversed String is : {reversedStr} \n");
+      
+    }
+}
+
